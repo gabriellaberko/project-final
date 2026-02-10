c2993cc3cb8dfa216e34f0843599ebbc526494d5
Gabriella Berkowicz <gabriellaberkowicz@MacBook-Air-som-tillhor-Gabriella.local>
2026-02-10 14:08:25 +0100

    Configure MongoDB and TypeScript

---
+++ backend/server.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import "dotenv/config";
+import listEndpoints from "express-list-endpoints";
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  const endpoints = listEndpoints(app);
+  res.json({
+    message: "Welcome to the Travel Planning API",
+    endpoints: endpoints,
+  });
+});
+
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/final-project";
+mongoose.connect(mongoUrl)
+  .then(() => { 
+    console.log("MongoDB Connected");
+    // Start the server
+    app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
+  })
+  .catch(err => console.error('MongoDB connection error:', err));
