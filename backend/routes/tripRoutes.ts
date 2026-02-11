891db0d6310507470fbffcefb40a893eb2469fca
Sandra <sandrahagevall@hotmail.com>
2026-02-11 16:55:11 +0100

    add route to add activity

--- backend/routes/tripRoutes.ts
+++ backend/routes/tripRoutes.ts
@@ -6,6 +6,10 @@ import { authenticateUser } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
+
+// TODO: a route to get all trips for view only when not authenticated. 
+
+
 // Post a new trip
 router.post("/", authenticateUser, async (req: Request, res: Response) => {
   try {
@@ -37,6 +41,39 @@ router.post("/", authenticateUser, async (req: Request, res: Response) => {
     res.status(400).json({
       success: false,
       message: "Failed to save trip to database",
+      error: err instanceof Error ? err.message : String(err)
+    });
+  }
+});
+
+
+// Route to delete a trip
+router.delete("/:tripId", authenticateUser, async (req: Request, res: Response) => {
+  try {
+    const { tripId } = req.params;
+
+    const deletedTrip = await Trip.findOneAndDelete({
+      _id: tripId,
+      creator: req.user!._id
+    });
+
+    if (!deletedTrip) {
+      return res.status(404).json({
+        success: false,
+        message: "Trip not found or not authorized"
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      response: deletedTrip,
+      message: "Trip deleted successfully"
+    });
+
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Failed to delete trip",
       error: err instanceof Error ? err.message : String(err)
     });
   }
@@ -89,6 +126,7 @@ router.post("/:tripId/days", authenticateUser, async (req: Request, res: Respons
   }
 });
 
+
 // Route to delete a day
 router.delete("/:tripId/days/:dayId", authenticateUser, async (req: Request, res: Response) => {
   try {
@@ -137,6 +175,64 @@ router.delete("/:tripId/days/:dayId", authenticateUser, async (req: Request, res
     return res.status(500).json({
       success: false,
       message: "Failed to delete day",
+      error: err instanceof Error ? err.message : String(err)
+    });
+  }
+});
+
+// Route to add actitivty
+router.post("/:tripId/days/:dayId/activities", authenticateUser, async (req: Request, res: Response) => {
+  try {
+    const { tripId, dayId } = req.params;
+    const { name, description, category, time, googleMapLink } = req.body;
+
+    const trip = await Trip.findById(tripId);
+
+    if (!trip) {
+      return res.status(404).json({
+        success: false,
+        response: null,
+        message: "Trip not found"
+      });
+    }
+
+    if (!trip.creator.equals(req.user!._id)) {
+      return res.status(403).json({
+        success: false,
+        message: "Not authorized"
+      });
+    }
+
+    const day = trip.days.id(dayId as any);
+
+    if (!day) {
+      return res.status(404).json({
+        success: false,
+        message: "Day not found"
+      });
+    }
+
+    const newActivity = {
+      name,
+      description,
+      category,
+      time,
+      googleMapLink
+    };
+
+    day.activities.push(newActivity);
+
+    const updatedTrip = await trip.save();
+    return res.status(200).json({
+      success: true,
+      response: updatedTrip,
+      message: "Activity added successfully"
+    })
+
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Failed to add activity",
       error: err instanceof Error ? err.message : String(err)
     });
   }
