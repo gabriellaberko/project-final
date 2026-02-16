9af9d3a0bc1445a07519c33d05046229237bfb82
Gabriella Berkowicz <gabriellaberkowicz@MacBook-Air-som-tillhor-Gabriella.local>
2026-02-16 16:39:09 +0100

    Remove test code

--- frontend/src/pages/AuthenticationPage.tsx
+++ frontend/src/pages/AuthenticationPage.tsx
@@ -1,13 +1,11 @@
 import { SignupForm } from "../components/forms/SignupForm";
 import { LoginForm } from "../components/forms/LoginForm";
-import { CreateActivityForm } from "../components/forms/CreateActivityForm";
 
 // TO DO: Fix this page
 export const AuthenticationPage = () => {
   return (
     <>
       <SignupForm />
-      <CreateActivityForm />
     </>
   )
 };
