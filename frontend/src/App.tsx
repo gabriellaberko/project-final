34d81bdf76625b4949ca3eeec1efaf243933cd26
Asako Kanno <asa5ng13@gmail.com>
2026-02-17 18:17:11 +0100

    style the form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { useAuthStore } from "./stores/AuthStore";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MyTripsPage } from "./pages/MyTripsPage";
 import { TripDetailsPage } from "./pages/TripDetailsPage";
+import { CreateTripForm } from "./components/forms/CreateTripForm";
 
 // TO DO: Create more pages and implement routing
 
@@ -16,6 +17,7 @@ export const App = () => {
 
   return (
     <BrowserRouter>
+      <CreateTripForm/>
       <Routes>
         <Route path="/mytrips" element={<MyTripsPage/>} />
         <Route path="/trip/:id" element={<TripDetailsPage/>} />
