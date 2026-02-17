aa456ab682c6e14242b9fb499f263eced02031d4
Gabriella Berkowicz <gabriellaberkowicz@MacBook-Air-som-tillhor-Gabriella.local>
2026-02-17 09:30:35 +0100

    Make login persist by calling chechAuthStatus in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
-import { AuthenticationPage } from "./pages/AuthenticationPage";
+import { useEffect } from "react";
+import { useAuthStore } from "./stores/AuthStore";
+
 
 // TO DO: Create more pages and implement routing
 
 export const App = () => {
 
+  const checkAuthStatus = useAuthStore(state => state.checkAuthStatus);
+  
+  useEffect(() => { 
+    checkAuthStatus();
+  }, [checkAuthStatus])
+
   return (
     <>
       <h1>Welcome to Final Project!</h1>
-      <AuthenticationPage />
     </>
   );
 };
