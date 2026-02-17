e3ade0bfee51ca30fbe8952c3e0445817e9d6663
Gabriella Berkowicz <gabriellaberkowicz@MacBook-Air-som-tillhor-Gabriella.local>
2026-02-17 10:23:50 +0100

    Create skeleton for pages

--- frontend/src/pages/TripDetailsPage.tsx
+++ frontend/src/pages/TripDetailsPage.tsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 export const TripDetailsPage = () => {
+
+  const { id: tripId } = useParams(); // Retrieve trip ID from the url
+
+  // TO DO: Fetch a specific trip from API, based on the tripID
+  useEffect(() => {
+    const fetchTrip = async () => {
+      try {
+
+      } catch (err) {
+        
+      }
+    }
+  }, [])
+
   return (
     <>
     </>
