6fb56194fb18a2a3904800a79026b2cac31412bf
Asako Kanno <asa5ng13@gmail.com>
2026-02-24 14:45:41 +0100

    make a component for an activity

---
+++ frontend/src/components/common/Activity.tsx
@@ -0,0 +1,55 @@
+import { useTripStore } from "../../stores/TripStore";
+import { useAuthStore } from "../../stores/AuthStore";
+import { ActivityInterface } from "../../types/interfaces";
+
+import Card from "@mui/joy/Card"
+
+interface ActivityProps {
+  tripId: string;
+  dayId: string;
+  activity: ActivityInterface;
+}
+
+export const Activity = ({ tripId, dayId, activity }: ActivityProps) => {
+  const isAuthenticated = useAuthStore(state => state.isAuthenticated);
+  const removeActivity = useTripStore(state => state.removeActivity)
+
+
+  return (
+    <>
+      <Card className="shadow-sm m-1">
+          <div className="flex flex-row justify-between gap-2">
+            <div className="flex flex-row items-center">
+              <div>
+                <span className="text-3xl">🍿</span> {/* TO DO: Change it to activity category icon*/}
+              </div>
+
+              <div className="flex flex-col gap-2 p-4 items-start">
+                {activity.name && <h4>{activity.name}</h4>}
+                {activity.description && <p>{activity.description}</p>}
+                {activity.time && <p><b>Time:</b> {activity.time}</p>}
+                {activity.googleMapLink &&
+                  <a 
+                    href={activity.googleMapLink} 
+                    target="_blank" 
+                    className="text-sm outline outline-[#837E7E] px-3 py-1 rounded-lg cursor-pointer"
+                  >
+                    Google Map Link
+                  </a>
+                }
+              </div>
+            </div>
+
+            {isAuthenticated &&
+              <button 
+                onClick={() => removeActivity(tripId, dayId, activity._id)}
+                className="self-start cursor-pointer"
+              >
+                🗑️
+              </button>
+            }
+          </div>         
+      </Card>
+    </>
+  )
+}
--- frontend/src/components/common/DayCard.tsx
+++ frontend/src/components/common/DayCard.tsx
@@ -3,45 +3,52 @@ import { useTripStore } from "../../stores/TripStore";
 import { MainBtn } from "../buttons/MainBtn";
 import { DayCardProps } from "../../types/interfaces";
 import { useAuthStore } from "../../stores/AuthStore";
-
+import { Activity } from "../common/Activity"
+import Card from "@mui/joy/Card"
 
 export const DayCard = ({ day }: DayCardProps) => {
   const navigate = useNavigate();
   const trip = useTripStore(state => state.trip);
   const isAuthenticated = useAuthStore(state => state.isAuthenticated);
   const removeDay = useTripStore(state => state.removeDay);
-  const removeActivity = useTripStore(state => state.removeActivity);
+
+  if (!trip) return null;
 
   return (
     <>
       <div className="text-center flex flex-col items-center">
         <div>
-          <h2 className="self-center">Day {day.dayNumber}</h2>
-          <div key={day.dayNumber} className="flex flex-col w-max-349 h-max-786 p-8 mx-12 rounded-[14px] shadow-md md:mx-0">
+          <h2 className="self-center m-2">Day {day.dayNumber}</h2>
+
+          <Card key={day.dayNumber} className="flex flex-col w-max-349 h-max-786 p-8 mx-12 shadow-md md:mx-0">
             <div className="flex flex-col w-full justify-evenly">
               {isAuthenticated &&
-                <button onClick={() => removeDay(trip!._id, day._id)} className="self-end cursor-pointer text-xl">x</button>
+                <button 
+                  onClick={() => removeDay(trip!._id, day._id)} 
+                  className="self-end cursor-pointer text-xl"
+                >
+                  x
+                </button>
               }
             </div>  
+
             <div className="flex flex-col md:items-stretch gap-2 my-4">
                 <h3>Activities</h3>
-                {day.activities.map((activity, index) => (
-                  <div key={index} className="flex flex-col gap-2 shadow-sm p-4 items-start">
-                    {isAuthenticated &&
-                      <button onClick={() => removeActivity(trip!._id, day._id, activity._id)} className="self-end cursor-pointer">x</button>
-                    }
-                    {activity.name && <h4><b>Name:</b> {activity.name}</h4>}
-                    {activity.description && <p><b>Description:</b> {activity.description}</p>}
-                    {activity.category && <p><b>Category:</b> {activity.category}</p>}
-                    {activity.time && <p><b>Time:</b> {activity.time}</p>}
-                    {activity.googleMapLink &&
-                      <a href={activity.googleMapLink} target="_blank" className="outline outline-[#837E7E] p-1 rounded-lg">
-                        Google Map Link
-                      </a>
-                    }
-                  </div>
-                ))}
+
+                  {day.activities.length > 0 ? (
+                    day.activities.map((activity) => (
+                      <Activity 
+                        key={activity._id} 
+                        tripId={trip!._id} 
+                        dayId={day._id} 
+                        activity={activity}
+                      />
+                    ))
+                  ) : (
+                    <p>No activities yet</p>
+                  )}
             </div>
+
             <div className="flex justify-center">
             {isAuthenticated &&
               <div>
@@ -49,7 +56,7 @@ export const DayCard = ({ day }: DayCardProps) => {
               </div>
             }
             </div>
-          </div>
+          </Card>
         </div>
       </div>
     </>
