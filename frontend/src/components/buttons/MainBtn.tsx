ced7b833b9dcde79fe6d37a67a19b6f0713f22ab
Asako Kanno <asa5ng13@gmail.com>
2026-02-26 11:54:41 +0100

    change mainbtn

--- frontend/src/components/buttons/MainBtn.tsx
+++ frontend/src/components/buttons/MainBtn.tsx
@@ -1,17 +1,16 @@
 import { BtnProps } from "../../types/interfaces";
 
 
-export const MainBtn = ({ children, onClick }: BtnProps) => {
+export const MainBtn = ({ children, onClick, className = "" }: BtnProps) => {
   return (
     <button
-      className="
-        bg-[#837E7E] 
-        text-white
-        flex items-center justify-center
-        md:rounded-lg md:px-4 md:py-2
-        rounded-full w-12 h-12
-        md:w-auto md:h-auto
-      "
+      className={[
+        "bg-[#837E7E] text-white flex items-center justify-center",
+        "md:rounded-lg md:px-4 md:py-2",
+        "rounded-full w-12 h-12 md:w-auto md:h-auto", 
+        "cursor-pointer",
+        className,
+      ].join(" ")}
       onClick={onClick}>
       {/* Mobile: show plus */}
       <span className="block md:hidden text-3xl font-bold leading-none">+</span>
